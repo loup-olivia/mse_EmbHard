a81a5ae47a4ef7dde140fef71b8f68863e74e95c
olivia-loup <olivia.loup@heig-vd.ch>
2024-10-26 15:53:20 +0200

    need verif eclipse

--- files/INT_LCD.vhd
+++ files/INT_LCD.vhd
@@ -20,26 +20,26 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 ENTITY INT_LCD IS
- GENERIC(N : NATURAL := 32);
+ GENERIC(N : NATURAL := 16);
  PORT(
 	-- Avalon interfaces signals
 		Clk_i 			: IN std_logic;
 		nReset_i 		: IN std_logic;
-		Address_i 		: IN std_logic_vector (2 DOWNTO 0);
+		Address_i 		: IN std_logic;
 		ChipSelect_i 	: IN std_logic;
 		Write_i 		: IN std_logic;
 		WriteData_i 	: IN std_logic_vector (N-1 DOWNTO 0);
 		
 		waitRequest_o	: OUT std_logic;
 		
-		-- 8080 interfaces
-		CSn_o			: OUT std_logic;
+	-- 8080 interfaces
+		-- CSn_o			: OUT std_logic;
 		D_Cn_o			: OUT std_logic;
 		DB_o			: OUT std_logic_vector (N-1 DOWNTO 0);
-		WRn_o			: OUT std_logic;
+		WRn_o			: OUT std_logic
 		-- RD_n_o			: OUT std_logic;
-		LCD_RESET_n_i	: OUT std_logic;
-		IM0_i			: OUT std_logic;
+		-- LCD_RESET_n_i	: OUT std_logic;
+		-- IM0_i			: OUT std_logic;
 	);
 End INT_LCD;
 
@@ -48,11 +48,11 @@ ARCHITECTURE comp OF INT_LCD IS
     -- Components (Nomenclature : name of the component + _c)
     -- Types (Nomenclature : name of the type + _t)
     -- exemple : type state_t is (idle, start, stop);
-	type state_t is (S1, S2, S3);
+		type state_t is (IDLE, S1, S2, S3);
     -- Signals (Nomenclature : name of the signal + _s)
     -- exemple : signal a : signed(N_bit-1 downto 0);
     signal DBreg_s 		: std_logic_vector (N-1 DOWNTO 0);
-	signal DC_s 		: std_logic;  
+	signal DCn_s 		: std_logic;  
 	signal statePres_s 	: state_t;
 	signal stateFut_s 	: state_t;
     -- Procedures (Nomenclature : name of the procedure + _p)
@@ -60,37 +60,52 @@ ARCHITECTURE comp OF INT_LCD IS
 begin
     -- Declarations
     -- Process
-    
-	pRegWr : 
-	process()
+    -- Avalon bus
+	RegWr_p : 
+	process(Clk_i,nReset_i)
 	begin
-	  if ='0' then
-
-	  elsif rising_edge(Clk) then
+	  if nReset_i ='0' then
+		statePres_s <= IDLE;
+		DBreg_s <= (others => '0');
+		DCn_s <= '0';
+	  elsif rising_edge(Clk_i) then
+			statePres_s <= stateFut_s;
+			DCn_s <= Address_i;
+			DBreg_s <= WriteData_i;
 
 	  end if; 
-	end process pRegWr;
+	end process RegWr_p;
  
-	pRegRd: 
-	process()
+	StateMa_p: 
+	process(ChipSelect_i,Write_i,statePres_s)
 	begin
-		case Address(2 downto 0) is
-			when S1;
-				CSn_o <= '1';
-				WRn_o <= '1';
-				waitRequest_o <= '1';
-			when S2;
-				CSn_o <= '0';
+		WRn_o <= '1';
+		waitRequest_o <= '1';
+		stateFut_s <= statePres_s;
+		case statePres_s is
+			when IDLE =>
+				if (ChipSelect_i <= '1' and Write_i <= '1') then
+					stateFut_s <= S1;
+				end if;
+			when S1 =>
 				WRn_o <= '0';
+				stateFut_s <= S2;
+			when S2 =>
+				stateFut_s <= S3; 
+			when S3 =>
 				waitRequest_o <= '0';
-
-			when S3;
-				waitRequest_o <= '1   ';
-			when others => null;
+				stateFut_s <= IDLE;
+			when others => 
+				stateFut_s <= IDLE;
 		end case;
-
 	end process ;
     
 	-- 8080
-
+	-- CSn_o			<= '0';
+	D_Cn_o			<= DCn_s;
+	DB_o			<= DBreg_s;
+	-- WRn_o			<= '0';
+	-- RD_n_o		<= '0';
+	-- LCD_RESET_n_i	<= nReset_i;
+	-- IM0_i			<= '0';
 END comp;
